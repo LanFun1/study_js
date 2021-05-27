447bf3954466f5ae231044b46a976494c1471acf
Vaydich Daniel <vik-vay.av@yandex.ru>
2021-05-27 21:52:19 +0300

    Создал index.html и script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let money = 5;
+let income = 10;
+let addExpenses = 23;
+let deposit = 12;
+let mission = "to do";
+let period = "12 month";
+
+alert("Я сделяль!");
+console.log("Здесь тоже сделяль!");  
